b713f27a1214fab1d0197ecf637be2ec37dc4d6d
Nicolas <nicolascardoso99@gmail.com>
2020-04-23 10:12:22 +0200

    Demande date jour heure sans php V2

--- pageSeeProfile.php
+++ pageSeeProfile.php
@@ -16,9 +16,9 @@ include "navbar.php";
 
 <?php
 // if don't have session user..then
-if(! isset($_SESSION['username']))
+if(! isset($_SESSION['nom']))
 {
-    die("<script>alert('Please login first before proceed!');window.location.href=pageLogin.php;</script>");
+    die("<script>alert('Je ne sais pas qui vous êtes, il faut se login !');window.location.href=pageLogin.php;</script>");
 }
 ?>
 
@@ -29,50 +29,54 @@ if(! isset($_SESSION['username']))
 </header>
 
 <div id="seeProfile">
-    <h1>Here is your profile</h1>
+    <h1>Voici votre profil</h1>
 
     <?php
     include "conn.php";
 
 
-    $sql="SELECT * FROM customer WHERE custUsername = '".$_SESSION['username']."'";
+    $sql="SELECT * FROM patient WHERE Nom = '".$_SESSION['nom']."'";
     $result = mysqli_query($conn, $sql);
 
     $rows = mysqli_fetch_array($result);
 
-        echo "<a>"."Username : ".$rows['custUsername']."</a><br/><br/>";
-        echo "<a>"."Password : ".$rows['custPassword']."</a><br/><br/>";
-        echo "<a>"."Email : ".$rows['custEmail']."</a><br/><br/>";
-        echo "<a>"."Phone Number : ".$rows['custPhone']."</a><br/><br/>";
-        echo "<a>"."Mailing Address : ".$rows['custMailing']."</a><br/><br/>";
-        echo "<a>"."Date of Birth : ".$rows['custDateOfBirth']."</a><br/><br/>";
-        echo "<a class='button' href='pageEditProfile.php?id=".$rows['custID']."'>Edit</a>";
+        echo "<a>"."Nom : ".$rows['Nom']."</a><br/><br/>";
+        echo "<a>"."Prénom : ".$rows['Prenom']."</a><br/><br/>";
+        echo "<a>"."Catégorie Sociale : ".$rows['Categorie_sociale']."</a><br/><br/>";
+        echo "<a>"."Email : ".$rows['Email']."</a><br/><br/>";
+        echo "<a>"."Comment m'avez-vous connue : ".$rows['Moyen_connaissance']."</a><br/><br/>";
+        echo "<a class='button' href='pageEditProfile.php?id=".$rows['ID_patient']."'>Modifier votre profil</a>";
 
     ?>
 </div>
 
 <div class="rents">
-    <h1>Your rents</h1>
+    <h1>Demander un rendez-vous</h1>
+
+    <?php
+    echo "<a class='button' href='pageRDV.php?id=".$rows['ID_patient']."'>Demander un rendez-vous</a>";
+    ?>
+</div>
+
+<div class="rents">
+    <h1>Vos rendez-vous</h1>
 
     <?php
 
-    $sql1 = "SELECT * FROM rent WHERE custID = '".$_SESSION['id']."'";
+    $sql1 = "SELECT * FROM rendez_vous WHERE ID_patient = '".$_SESSION['id']."'";
     $result1 = mysqli_query($conn, $sql1);
 
     while($rows = mysqli_fetch_array($result1)) {
 
-        $sql2 = "SELECT gameName FROM game WHERE gameID = '".$rows['gameID']."'";
-        $result2 = mysqli_query($conn, $sql2);
-        $rows1 = mysqli_fetch_array($result2);
-
         echo "<br/><br/>";
-        echo "<a>" . "Game Name : " . $rows1['gameName'] . "</a><br/><br/>";
-        echo "<a>" . "Begin Date : " . $rows['beginDate'] . "</a><br/><br/>";
-        echo "<a>" . "End Date : " . $rows['endDate'] . "</a><br/><br/>";
+        echo "<a>" . "Date : " . $rows['Date'] . "</a><br/><br/>";
+        echo "<a>" . "Heure : " . $rows['Heure'] . "</a><br/><br/>";
     }
 
     ?>
+
 </div>
+
 </body>
 
 </html>
