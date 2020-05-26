57910fe5df3105e7b070e1d18b76cc6c6aed2827
Nicolas <nicolascardoso99@gmail.com>
2020-05-26 20:17:49 +0200

    Opti de la navigation de la psy

--- navbar.php
+++ navbar.php
@@ -1,28 +1,28 @@
 <?php
 
-    function navbar()
+function navbar()
+{
+    echo "<ul>";
+
+    if(! isset($_SESSION['id']))
     {
-        echo "<ul>";
+        echo "<li><a href='pageLogin.php'>Login</a></li>";
+        echo "<li><a href='pageRegister.php'>S'enregistrer</a></li>";
+    }
+    else if($_SESSION['role'] === "0")
+    {
+        echo "<li><a href='pageSeeProfile.php'>Profil</a></li>";
+        echo "<li><a href='functionLogout.php'>Logout</a></li>";
+    }
+    else
+    {
+        echo "<li><a href='pageViewData.php'>View Data</a></li>";
+        echo "<li><a href='nouveauPatient.php'>Ajouter un nouveau client</a></li>";
+        echo "<li><a href='pageValiderRDV.php'>Demandes de RDV clients</a></li>";
+        echo "<li><a href='calendrier.php'>Planning</a></li>";
+        echo "<li><a href='functionLogout.php'>Logout</a></li>";
+    }
 
-        if(! isset($_SESSION['id']))
-        {
-            echo "<li><a href='pageLogin.php'>Login</a></li>";
-            echo "<li><a href='pageRegister.php'>S'enregistrer</a></li>";
-        }
-        else if($_SESSION['role'] === "0")
-        {
-            echo "<li><a href='pageSeeProfile.php'>Profil</a></li>";
-            echo "<li><a href='functionLogout.php'>Logout</a></li>";
-        }
-        else
-        {
-            echo "<li><a href='pageViewData.php'>View Data</a></li>";
-            echo "<li><a href='nouveauPatient.php'>Ajouter un nouveau client</a></li>";
-            echo "<li><a href='calendrier.php'>Planning</a></li>";
-            echo "<li><a href='pageValiderRDV.php'>Demandes de RDV clients</a></li>";
-            echo "<li><a href='functionLogout.php'>Logout</a></li>";
-        }
-
-        echo "</ul>";
+    echo "</ul>";
 }
 ?>
