e3cea4374c44989d92fd87de34b5ab967a0482e0
lucasb8 <lucas.brire@efrei.net>
2020-05-30 11:34:34 +0200

    Suppression patient

--- functionDeletePatient.php
+++ functionDeletePatient.php
@@ -5,13 +5,23 @@ include "conn.php";
 $id = intval($_GET['id']);
 
 $sql = "DELETE FROM patient WHERE ID_patient = $id";
-$result = mysqli_query($conn, $sql);
+mysqli_query($conn, $sql);
 
 if(mysqli_affected_rows($conn)<=0)
 {
     echo "<script>alert('Impossible de supprimer les données!');";
     die ("window.location.href='pageViewData.php';</script>");
 }
+
+$sql2 = "DELETE FROM profession WHERE ID_patient = $id";
+mysqli_query($conn, $sql2);
+
+$sql3 = "DELETE FROM rendez_vous WHERE ID_patient = $id";
+mysqli_query($conn, $sql3);
+
+$sql4 = "DELETE FROM consultation WHERE ID_patient = $id";
+mysqli_query($conn, $sql4);
+
 
 mysqli_close($conn);
 
